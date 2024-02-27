90e21012fd02617e86feecd61d9cfc472ffd309d
Yong3pole <161507933+Yong3pole@users.noreply.github.com>
2024-02-28 05:16:40 +0800

    Update read.php

--- read.php
+++ read.php
@@ -1,10 +1,28 @@
 <?php
+    include 'connect.php';
 
-include 'connect.php';
+    // Check if ID is set and is valid
+    if (isset($_GET['deleteid'])) {
+        $ID = filter_var($_GET['deleteid'], FILTER_SANITIZE_NUMBER_INT);
 
+        // Delete the owner with the given ID
+        $stmt = mysqli_prepare($con, "DELETE FROM owners WHERE ID = ?");
+        mysqli_stmt_bind_param($stmt, "i", $ID);
+        mysqli_stmt_execute($stmt);
 
+        if (mysqli_stmt_affected_rows($stmt) > 0) {
+            echo '<script>window.location.href = "read.php";</script>';
+            exit;
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
+    }
+
+    if (isset($_POST['read_return'])) {
+        echo '<script>window.location.href = "index.php";</script>';
+        exit; // Exit immediately after redirecting to prevent further execution
+    }
 ?>
-
 
 <!doctype html>
 <html lang="en">
@@ -12,19 +30,15 @@ include 'connect.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <title>Display</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
-
-
-
-
 <body>
+    
     <div class="container my-5">
+        <h2>Students</h2>
         <table class="table">
             <thead>
                 <tr>
@@ -57,17 +71,18 @@ include 'connect.php';
                     <td>' . $email . '</td>
                     <td>' . $mobile . '</td>
                     <td>
-                    <a href="#" class="btn btn-dark">Update</a>
-                    <a href="#" class="btn btn-danger">Delete</a>
-
+                    <a href="update.php?updateid='.$ID.'" class="btn btn-primary btn-sm">Update</a>
+                    <a href="read.php?deleteid='.$ID.'" class="btn btn-danger btn-sm">Delete</a>
                     </td>
                 </tr>';
                 }
                 ?>
 
-
             </tbody>
         </table>
+        <form method="post">
+            <button class="btn btn-dark btn-lg my-5" name="read_return">Return</button>
+        </form>
     </div>
 
 </body>
