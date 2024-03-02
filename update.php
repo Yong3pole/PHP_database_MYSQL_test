ca8c4352755e86d743ead5d2c0524d8f5c718344
Yong3pole <161507933+Yong3pole@users.noreply.github.com>
2024-03-02 19:35:07 +0800

    Update update.php

--- update.php
+++ update.php
@@ -54,7 +54,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Update Owner</title>
+    <title>Update Customer Info</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -62,18 +62,18 @@
 <body>
     
     <div class="container my-5">
-        <h1>Update Info</h1>
+        <h1>Update Customer Info</h1>
         <form method="post">
             <div class="form-group mb-3 my-5">
                 <label for="exampleInputaddress1" class="form-label">First Name</label>
                 <input type="text" class="form-control" style="width: 400px" autocomplete="off" name="cname" value="<?php echo htmlspecialchars($cname); ?>">
             </div>
             <div class="form-group mb-3">
-                <label for="exampleInputaddress1" class="form-label">Last Name</label>
+                <label for="exampleInputaddress1" class="form-label">Phone</label>
                 <input type="text" class="form-control" style="width: 400px" autocomplete="off" name="phone" value="<?php echo htmlspecialchars($phone); ?>">
             </div>
             <div class="form-group mb-3">
-                <label for="exampleInputaddress1" class="form-label">address</label>
+                <label for="exampleInputaddress1" class="form-label">Address</label>
                 <input type="text" class="form-control" style="width: 400px" autocomplete="off" name="address" value="<?php echo htmlspecialchars($address); ?>">
             </div>
             
